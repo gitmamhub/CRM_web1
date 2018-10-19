0e4db3ea79956f1bd68bf782ad7c9cfc5c478774
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-10-19 10:56:25 -0400

    crm without delete option

--- contact.rb
+++ contact.rb
@@ -1,183 +1,199 @@
 
+  gem 'activerecord', '=4.2.10'
+  require 'active_record'
+  require 'mini_record'
 
+  ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm.sqlite3')
 
+  class Contact < ActiveRecord::Base
 
+    field :first_name, as: :string
+    field :last_name,  as: :string
+    field :email,      as: :string
+    field :note,       as: :text
 
-class Contact
-
-  @@contacts = []
-  @@id1= 1
-  # This method should initialize the contact's attributes
-    def initialize(first_name, last_name, email, notes)
-    @first_name = first_name
-    @last_name = last_name
-    @email = email
-    @notes = notes
-    @id = @@id1
-    @@id1 += 1
-  end
-
-  def first_name
-    return @first_name
-  end
-  def last_name
-    return @last_name
-  end
-  def id
-    return @id
-  end
-
-  def email
-    return @email
-  end
-
-  def notes
-    return @notes
-
-  end
-
-  def email=(email)
-    @email = email
-
-  end
-
-  def notes=(notes)
-      @notes = notes
-
-  end
-  # This method should call the initializer,
-  # store the newly created contact, and then return it
-
-    def self.create(first_name, last_name, email, notes = 'N/A')
-    new_contact = self.new(first_name, last_name, email, notes)
-
-    @@contacts << new_contact
-    return new_contact
+    # returns full name of the person
+    def full_name
+      return "#{first_name} #{last_name}"
     end
-
-  # This method should return all of the existing contacts
-  def self.all
-    return @@contacts
   end
+  Contact.auto_upgrade!
 
-  # This method should accept an id as an argument
-  # and return the contact who has that id
-  def self.find (id)
-    @@contacts.each do |info|
-         if (id == info.id)
-         return info
-         end
-       end
-  end
-
-  # This method should allow you to specify
-  # 1. which of the contact's attributes you want to update
-  # 2. the new value for that attribute
-  # and then make the appropriate change to the contact
-  def update(attribute, value )
-
-      # puts("which of the contact's attributes update ? : ")
-      # puts("<1>: first_name, <2>: last_name, <3>: email, <4>: notes ")
-      #
-      # m = gets
-      #
-      #
-      m = attribute
-      if m.to_i == 1
-        self.first_name = value
-      elsif m.to_i == 2
-        self.last_name = value
-      elsif m.to_i == 3
-        self.email = value
-      elsif m.to_i == 4
-        self.notes = value
-      end
-
-
-  end
-
-  # This method should work similarly to the find method above
-  # but it should allow you to search for a contact using attributes other than id
-  # by specifying both the name of the attribute and the value
-  # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by(new_attribute)
-
-# ...........
-  # puts "#attribute type is :#{new_attribute.class}"
-  # puts "#{@@contacts }"
-
-
-  case new_attribute.to_i
-
-  when 1 then   puts "Input first_name: "
-                first_name = gets.chomp
-                @@contacts.each do |info|
-                   if(first_name == info.first_name)
-                     p "The searched person is #{info.full_name}"
-
-                   end
-                end
-  when 2 then
-                puts "Input second_name: "
-                last_name = gets.chomp
-                @@contacts.each do |info|
-                   if(last_name == info.last_name)
-                     p "The searched person is #{info.full_name}"
-
-                   end
-                end
-
-  when 3 then
-                puts "Input email: "
-                email = gets.chomp
-                @@contacts.each do |info|
-                   if(email == info.email)
-                     p "The searched person is #{info.full_name}"
-
-                   end
-                end
-  when 4 then
-                puts "Input Id: "
-                id = gets.chomp.to_i
-                @@contacts.each do |info|
-                   if(id == info.id)
-                     p "The searched person is #{info.full_name}"
-
-                   end
-                end
-
-        else
-                puts "invalid choice !!!"
-
-  end
-# .............
-
-
-
-  end
-
-  # This method should delete all of the contacts
-  def self.delete_all
-      @@contacts.clear
-      return @@contacts
-
-  end
-
-  def full_name
-      display_name = "#{@first_name} #{@last_name}"
-      # p display_name
-      return display_name
-  end
-
-  # This method should delete the contact
-  # HINT: Check the Array class docs for built-in methods that might be useful here
-  def delete
-
-
-        @@contacts.delete(self)
-
-  end
-
-  # Feel free to add other methods here, if you need them.
-
-end
+#   @@contacts = []
+#   @@id1= 1
+#   # This method should initialize the contact's attributes
+#     def initialize(first_name, last_name, email, notes)
+#     @first_name = first_name
+#     @last_name = last_name
+#     @email = email
+#     @notes = notes
+#     @id = @@id1
+#     @@id1 += 1
+#   end
+#   def self.contacts
+#     return @@contacts
+#   end
+#   def first_name
+#     return @first_name
+#   end
+#   def last_name
+#     return @last_name
+#   end
+#   def id
+#     return @id
+#   end
+#
+#   def email
+#     return @email
+#   end
+#
+#   def notes
+#     return @notes
+#
+#   end
+#
+#   def email=(email)
+#     @email = email
+#
+#   end
+#
+#   def notes=(notes)
+#       @notes = notes
+#
+#   end
+#   # This method should call the initializer,
+#   # store the newly created contact, and then return it
+#
+#     def self.create(first_name, last_name, email, notes = 'N/A')
+#     new_contact = self.new(first_name, last_name, email, notes)
+#
+#     @@contacts << new_contact
+#     return new_contact
+#     end
+#
+#   # This method should return all of the existing contacts
+#   def self.all
+#     return @@contacts
+#   end
+#
+#   # This method should accept an id as an argument
+#   # and return the contact who has that id
+#   def self.find (id)
+#     @@contacts.each do |info|
+#          if (id == info.id)
+#          return info
+#          end
+#        end
+#   end
+#
+#   # This method should allow you to specify
+#   # 1. which of the contact's attributes you want to update
+#   # 2. the new value for that attribute
+#   # and then make the appropriate change to the contact
+#   def update(attribute, value )
+#
+#       # puts("which of the contact's attributes update ? : ")
+#       # puts("<1>: first_name, <2>: last_name, <3>: email, <4>: notes ")
+#       #
+#       # m = gets
+#       #
+#       #
+#       m = attribute
+#       if m.to_i == 1
+#         self.first_name = value
+#       elsif m.to_i == 2
+#         self.last_name = value
+#       elsif m.to_i == 3
+#         self.email = value
+#       elsif m.to_i == 4
+#         self.notes = value
+#       end
+#
+#
+#   end
+#
+#   # This method should work similarly to the find method above
+#   # but it should allow you to search for a contact using attributes other than id
+#   # by specifying both the name of the attribute and the value
+#   # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
+#   def self.find_by(new_attribute)
+#
+# # ...........
+#   # puts "#attribute type is :#{new_attribute.class}"
+#   # puts "#{@@contacts }"
+#
+#
+#   case new_attribute.to_i
+#
+#   when 1 then   puts "Input first_name: "
+#                 first_name = gets.chomp
+#                 @@contacts.each do |info|
+#                    if(first_name == info.first_name)
+#                      p "The searched person is #{info.full_name}"
+#
+#                    end
+#                 end
+#   when 2 then
+#                 puts "Input second_name: "
+#                 last_name = gets.chomp
+#                 @@contacts.each do |info|
+#                    if(last_name == info.last_name)
+#                      p "The searched person is #{info.full_name}"
+#
+#                    end
+#                 end
+#
+#   when 3 then
+#                 puts "Input email: "
+#                 email = gets.chomp
+#                 @@contacts.each do |info|
+#                    if(email == info.email)
+#                      p "The searched person is #{info.full_name}"
+#
+#                    end
+#                 end
+#   when 4 then
+#                 puts "Input Id: "
+#                 id = gets.chomp.to_i
+#                 @@contacts.each do |info|
+#                    if(id == info.id)
+#                      p "The searched person is #{info.full_name}"
+#
+#                    end
+#                 end
+#
+#         else
+#                 puts "invalid choice !!!"
+#
+#   end
+# # .............
+#
+#
+#
+#   end
+#
+#   # This method should delete all of the contacts
+#   def self.delete_all
+#       @@contacts.clear
+#       return @@contacts
+#
+#   end
+#
+#   def full_name
+#       display_name = "#{@first_name} #{@last_name}"
+#       # p display_name
+#       return display_name
+#   end
+#
+#   # This method should delete the contact
+#   # HINT: Check the Array class docs for built-in methods that might be useful here
+#   def delete
+#
+#
+#         @@contacts.delete(self)
+#
+#   end
+#
+#   # Feel free to add other methods here, if you need them.
+#
+# end
--- crm.rb
+++ crm.rb
@@ -5,71 +5,57 @@ require 'sinatra'
 
 
 get '/' do
-  erb :index
+  @contacts = Contact.all
+  redirect to ('/index')
 end
 
-get '/contacts' do
+get '/about' do
+  erb :about
+end
+
+get '/index' do
+    erb :index
+end
+
+get '/contacts' do #creating an instance variable of all contact in the list
   @contacts = Contact.all
   erb :contacts
 end
 
 get '/contacts/new' do
-
   erb :new
 end
 
-# get '/contacts/:id' do
-#   #        = Contact.find_by(id: 1/2/3/4 whatever the url is taking)/
-#   #@contact =
-#   @contact = Contact.find_by({id: params[:id].to_i})
-#   if @contact
-#     erb :show_contact
-#   elsif
-#     raise Sinatra::NotFound
-#   end
-# end
-#
-# get '/about' do
-#   erb :about
-# end
-#
-# get '/contacts/:id/edit' do
-#   @contact = Contact.find_by(id: params[:id].to_i)
-#   if @contact
-#     erb :edit_contact
-#   else
-#     raise Sinatra::NotFound
-#   end
-# end
-#
-# post '/contacts' do
-#   Contact.create(
-#     first_name: params[:first_name],
-#     last_name:  params[:last_name],
-#     email:      params[:email],
-#     note:       params[:note]
-#   )
-#   redirect to('/contacts')
-# end
-#
-# put '/contacts/:id' do
-#   @contact = Contact.find_by(id: params[:id].to_i)
-#   if @contact
-#     @contact.update(
-#       first_name: params[:first_name],
-#       last_name:  params[:last_name],
-#       email:      params[:email],
-#       note:       params[:note]
-#     )
-#
-#     redirect to ('/contacts')
-#   else
-#     raise Sinatra::NotFound
-#   end
-# end
-#
+get '/contacts/:id'do # finding a contact
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+    raise Sinetra::NotFound
+  end
+end
 
+post '/contacts' do # creates a new contact
+  Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
 
-# after do
-#   ActiveRecord::Base.connection.close
-# end
+  redirect to ('/contacts')
+end
+
+delete '/contacts/:id' do
+  @contact  = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to ('/contacts')
+  else
+    raise Sinatra::NotFound
+end
+end
+
+after do
+  ActiveRecord::Base.connection.close
+end
